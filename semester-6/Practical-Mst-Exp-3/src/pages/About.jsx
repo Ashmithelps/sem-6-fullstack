37f157f697e47832991138df51d63d31f00fce23
ashmithelps <thakurashmit20@gmail.com>
2026-03-10 12:11:30 +0530

    ongoing practical MST

---
+++ semester-6/Practical-Mst-Exp-3/src/pages/About.jsx
@@ -0,0 +1,11 @@
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This page explains about our React application.</p>
+     <h1> <p>Curated by Ashmit Thakur</p></h1>
+    </div>
+  );
+}
+
+export default About;
---
+++ semester-6/Practical-Mst-Exp-3/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to the React Routing Example.</p>
+       <h1> <p>Curated by Ashmit Thakur</p></h1>
+    </div>
+  );
+}
+
+export default Home;
